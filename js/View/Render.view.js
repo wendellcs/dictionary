7445c2126bccd35f2bffc6603a59f4f8b8d79862
Wendell <wendellcs131@gmail.com>
2024-04-14 13:04:07 -0300

    Update RenderView

--- js/View/Render.view.js
+++ js/View/Render.view.js
@@ -9,57 +9,56 @@ class RenderView {
     }
 
     renderWords(words) {
+        this.toBeRendered = 5
+
         if (words.length > 0) {
             const index = words.length - 1;
-            // this.containerResults.innerHTML = '';
-
             this.containerResults.classList.remove('hidden')
+
+            // Get important HTML elements
             const title = this.containerResults.querySelector('.title')
             const select = this.containerResults.querySelector('.container-results-select')
             const wordClass = this.containerResults.querySelector('.word-class')
-
-
 
             this.words = words
             this.select = select
             this.index = index
 
             select.textContent = ''
+            // Get all word classes based on the chosen word
             const classes = words[index].wordClasses.map((_class) => _class)
+            // Update the title
             title.textContent = words[index].word
-            // Remove any repeated word
+
+            // Remove any repeated word class
             const toBeRenderedClasses = classes.filter((_class, i) => {
                 return classes.indexOf(_class) === i
             })
 
             // Create the options based on the word classes of the chosen word.
             toBeRenderedClasses.forEach((_class) => {
-                if (!select.querySelector(`option.${_class}`)) {
-                    const option = document.createElement('option');
-                    option.value = _class;
-                    option.innerText = _class;
-                    option.className = 'option'
-                    select.appendChild(option);
-                }
+                const option = document.createElement('option');
+                option.value = _class;
+                option.innerText = _class;
+                option.className = 'option'
+                select.appendChild(option);
             })
 
-            // Update the definitions based on the selected word
+            // Update the definitions once the user has chosen a different word class
             select.addEventListener('change', () => {
                 wordClass.textContent = select.value
                 this.toBeRendered = 5
-                this.loadDefinitions()
+                this.getDefinitionsReadyToBeRendered()
             })
-
-            this.loadDefinitions()
+            this.getDefinitionsReadyToBeRendered()
         }
     }
 
-    loadDefinitions() {
-        const results = this.containerResults.querySelector('.select-results')
+    getDefinitionsReadyToBeRendered() {
         const _class = this.select.value
         const wordClassIndex = []
-        const btnShowMore = this.containerResults.querySelector('.btn.show-more')
 
+        // Get the indexes where the words are repeated
         this.words[this.index].wordClasses.forEach((_wordClass, i) => {
             if (_wordClass == _class) {
                 wordClassIndex.push(i)
@@ -68,56 +67,50 @@ class RenderView {
 
         // Verify whether there are multiple definitions for the same word
         if (wordClassIndex.length > 1) {
-            results.textContent = ''
             const multipleResults = []
 
+            // Insert the repeated definitions into the same array
             wordClassIndex.forEach(i => {
                 multipleResults.push(this.words[this.index].definitions[i])
             })
 
+            // Make an unique array from the multipleResults array
             const rendering = []
             multipleResults.map(obj => { obj.map(def => { rendering.push(def) }) })
 
-            if (rendering.length > 5) {
-                btnShowMore.classList.remove('hidden')
-            }
-
-            for (let i = 0; i < this.toBeRendered; i++) {
-                if (rendering[i]) {
-                    const p = document.createElement('p')
-                    p.className = 'definition'
-                    p.textContent = rendering[i].definition
-                    results.appendChild(p)
-                } else {
-                    btnShowMore.classList.add('hidden')
-                }
-            }
-
+            this.renderDefinitions(rendering)
         } else {
-            results.textContent = ''
             const toBeRenderedDefinition = this.words[this.index].definitions[wordClassIndex]
 
-            if (toBeRenderedDefinition.length > 5) {
-                btnShowMore.classList.remove('hidden')
-            }
+            this.renderDefinitions(toBeRenderedDefinition)
+        }
+    }
 
-            for (let i = 0; i < this.toBeRendered; i++) {
-                if (toBeRenderedDefinition[i]) {
-                    const p = document.createElement('p')
-                    p.className = 'definition'
-                    p.textContent = toBeRenderedDefinition[i].definition
-                    results.appendChild(p)
-                } else {
-                    btnShowMore.classList.add('hidden')
-                }
-            }
+    renderDefinitions(definitionsList) {
+        const btnShowMore = this.containerResults.querySelector('.btn.show-more')
+        const results = this.containerResults.querySelector('.select-results')
+        results.textContent = ''
+
+        if (definitionsList.length > 5) {
+            btnShowMore.classList.remove('hidden')
         }
 
+        for (let i = 0; i < this.toBeRendered; i++) {
+            if (definitionsList[i]) {
+                const p = document.createElement('p')
+                p.className = 'definition'
+                p.textContent = definitionsList[i].definition
+                results.appendChild(p)
+            } else {
+                btnShowMore.classList.add('hidden')
+                break
+            }
+        }
     }
 
     showMore() {
         this.toBeRendered += 5
-        this.loadDefinitions()
+        this.getDefinitionsReadyToBeRendered()
     }
 
     toggleHistoryMenuVisibility(menu) {
