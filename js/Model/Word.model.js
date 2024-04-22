2a2c19a64d6b863729dd334c39f77d9524999645
Wendell <wendellcs131@gmail.com>
2024-04-22 17:36:50 -0300

    Update WordModel

--- js/Model/Word.model.js
+++ js/Model/Word.model.js
@@ -1,12 +1,12 @@
 class WordModel {
-    constructor({ word, definitions, wordClasses, examples }) {
-        if (!word || !definitions || !wordClasses || !examples) {
-            throw new Error('word, definitions, meaning and examples are required')
+    constructor({ word, definitionsByWordClass, phonetic, phoneticAudios }) {
+        if (!word || !definitionsByWordClass || !phonetic || !phoneticAudios) {
+            throw new Error('These datas are required')
         }
 
         this.word = word;
-        this.definitions = definitions;
-        this.wordClasses = wordClasses;
-        this.examples = examples;
+        this.definitionsByWordClass = definitionsByWordClass;
+        this.phonetic = phonetic;
+        this.phoneticAudios = phoneticAudios;
     }
 }
